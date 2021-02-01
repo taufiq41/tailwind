c0e1cb7039fca99eea285401cbe3a9c90f4f18e3
Triastono Taufiq <triastonotaufiq35@gmail.com>
2021-02-02 02:48:56 +0700

    change gulpfile settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
-// var gulpUglify = require('gulp-uglify');
 
 gulp.task('default', async function () {
   gulp
    .src(['./node_modules/alpinejs/dist/*.js'])
-//    .pipe(gulpUglify())
    .pipe(gulp.dest('./public/dist/alpinejs/'));
 });
